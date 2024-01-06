1194c1564c53a6dae7bae934fa20772d4111591a
selfcoded <haolovelyjiang@gmail.com>
2024-01-06 21:13:20 +0100

    Login authentication

--- simple_backend/views.py
+++ simple_backend/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
+
+def home(request):
+    # validation of the users
+    if request.method == 'POST':
+        username = request.POST['user_name']
+        password = request.POST['password']
+        #Authenticate
+        user = authenticate(request, username = username, password = password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'you have logged in!')
+            return redirect('home')
+        else:
+            messages.success(request, 'there was an error....')
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
