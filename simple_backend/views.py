35d7795a6ad4a97535ce00cabc48e6c62a82dc6f
selfcoded <haolovelyjiang@gmail.com>
2024-01-06 22:36:59 +0100

    registration

--- simple_backend/views.py
+++ simple_backend/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .form import SignUpForm
 
 # Create your views here.
 
@@ -25,3 +26,19 @@ def logout_user(request):
     logout(request)
     messages.success(request, 'you have been logged out..')
     return redirect('home')
+
+def register(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username = username, password = password)
+            login(request, user)
+            messages.success(request, 'you have successfully registered!')
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form': form})
+    return render(request, 'register.html', {'form': form})
